44c978b82b09715c1abb4fa8eab3934f6f2b72a5
brody715 <brody71517@gmail.com>
2023-04-18 14:23:22 +0800

    chore: remove poetry, use setuptools

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ztrack',
+    version='0.0.1',
+    author='brody715',
+    author_email='brody71517@gmail.com',
+    description='',
+    url='https://github.com/brody715/ztrack',
+    packages=find_packages(),
+
+    tests_require=[
+        'pytest>=7.2.0',
+    ],
+    python_requires='>=3.8'
+)
